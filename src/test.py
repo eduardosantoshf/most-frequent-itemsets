7726b6739d7ab023c212aa1a7fe5cb6bf3115a18
Eduardo Santos <eduardosantoshf@gmail.com>
2023-03-09 09:05:31 +0000

    Added news dataset

--- src/test.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: Eduardo Santos
-# @Date:   2023-02-27 11:49:19
-# @Last Modified by:   Eduardo Santos
-# @Last Modified time: 2023-02-27 11:49:26
-
-import pyspark
-from pyspark import SparkContext
-sc = SparkContext()
